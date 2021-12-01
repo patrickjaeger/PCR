2cc17ee6ef51a23b4112b6da908c4e1e7eecb605
Patrick Jaeger <quabop@gmail.com>
2021-12-01 11:59:42 +0100

    add working conditional feedback

--- mod_40_gene-expr.R
+++ mod_40_gene-expr.R
@@ -2,14 +2,16 @@
 
 calculationUI <- function(id) {
   tagList(
-    
+    shinyFeedback::useShinyFeedback(),
     h3('Calculate Stuff'),
     selectInput(NS(id, 'housek'), 
                 'Select housekeeper:',
                 choices = c()),
-    textInput(NS(id, 'ctrl_grp'),
-                'Enter name of control group (case sensitive):'),
-    actionButton(NS(id, 'calculate'), 'Calculate Relative Gene Expression'),
+    strong('Enter name of control group (case sensitive):'),
+    textInput(NS(id, 'ctrl_grp'), NULL),
+    # actionButton(NS(id, 'check_ctrl_grp'), 'Validate control group'), 
+    actionButton(NS(id, 'calculate'), 
+                 'Calculate Relative Gene Expression'), br(),
     downloadButton(NS(id, 'd_res_long'), 'Download Results (long format)'),
     downloadButton(NS(id, 'd_res_wide'), 'Download Results (wide format)'),
     tableOutput(NS(id, 'head_res_long'))
@@ -26,18 +28,37 @@ calculationServer <- function(id, .dat) {
       updateSelectInput(inputId = 'housek', choices = unique(.dat()$target))
     })
     
-    # Get tags for control group
-    # 1. get all tag
-    all_tags <- reactive()
-    
-    
-    housek <- reactive(input$housek)
+    # Check and save control group
     ctrl_grp <- reactive({
-      # TODO Insert shinyFeedback if ctrl_grp cannot be found
-      input$ctrl_grp
-      })
+      shinyFeedback::feedbackWarning('ctrl_grp', 
+                                     str_length(input$ctrl_grp) == 0, '-_-')
+      req(!str_length(input$ctrl_grp) == 0, cancelOutput = TRUE)
+      
+      pass <- sum(str_detect(.dat()$sample, input$ctrl_grp)) > 0
+      
+      if (pass) {
+        shinyFeedback::hideFeedback('ctrl_grp')
+        shinyFeedback::showFeedbackSuccess('ctrl_grp', "Entry matched")
+        input$ctrl_grp
+      } else {
+        shinyFeedback::hideFeedback('ctrl_grp')
+        shinyFeedback::showFeedbackWarning('ctrl_grp', "Entry not found")
+      }
+    })
     
+    # Calculate gene expression
+    rel_gene_expr <- eventReactive(input$calculate, {
+      req(ctrl_grp())
+
+      .dat() %>% 
+        calc_dcq(input$housek) %>% 
+        calc_ddcq_and_fold_change(ctrl_grp())
+    })
     
+    # Render head of 
+    output$head_res_long <- renderTable({
+      head(rel_gene_expr())
+    })
     
     
   })
--- universal_separator.R
+++ universal_separator.R
@@ -1,2 +1,11 @@
 # How to split camelCase
 # https://stackoverflow.com/questions/8406974/splitting-camelcase-in-r
+
+s1 <- c("322_WT_1", "322_WT_2", "323_WT_1", "323_WT_2", "324_KO_1")
+
+strsplit(gsub("([[:upper:]])", " \\1", s1), " ")
+
+
+s1 <- '336_KO_1'
+str_detect(s1, 'KO')
+sum(str_detect(s1, 'WT')) > 0
